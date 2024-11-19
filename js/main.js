9871726b9c464c7d543763d7d7b3ce5322672ad7
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2024-11-19 10:53:18 +0100

    new paths at router

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 import { registerHandler } from "./handlers/auth/registerHandler.js";
 
 function router() {
-  const pathname = window.location.pathname;
-
-  console.log(pathname);
+  const fullPath = window.location.pathname;
+  const pathParts = fullPath.split("/").slice(2).join("/");
+  const pathname = `/${pathParts}`;
 
   switch (pathname) {
     case "/":
     case "/index.html":
-      console.log("Blog Post Public Page");
+      console.log("Blog Feed Page");
       break;
     case "/home/index.html":
       console.log("Homepage");
       break;
     case "/account/login.html":
-      console.log("Login");
+      console.log("Login Page");
       break;
-    case "../account/register/":
-      console.log("Registerpage");
+    case "/about/index.html":
+      console.log("Aboutpage");
+      break;
+    case "/post/index.html":
+      console.log("Blog Post Public Page");
+      break;
+    case "/post/manage.html":
+      console.log("Manage Posts Page");
+      break;
+    case "/post/create.html":
+      console.log("Create Post Page");
+      break;
+    case "/post/edit.html":
+      console.log("Edit Post Page");
+      break;
+    case "/account/register.html":
+      console.log("Register Page");
       registerHandler;
       break;
   }
