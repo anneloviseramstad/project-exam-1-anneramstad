dc873950d7b742919dbebeec916589cdbc09dfbb
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2024-11-28 16:48:29 +0100

    small touch ups in router and html

--- js/main.js
+++ js/main.js
@@ -18,53 +18,38 @@ function router() {
   loginStatusAndNavStatus();
 
   if (postId) {
-    console.log("Post details page");
     getPostByIdHandler(postId);
     return;
   }
 
   switch (pathname) {
     case "/":
-      console.log("Blog Feed Page");
       managePostsHandler();
       break;
     case "/home":
-      console.log("Homepage");
       break;
     case "/login":
-      console.log("Login Page");
       initLogin();
       break;
     case "/about":
-      console.log("Aboutpage");
       break;
     case "/post":
-      console.log("Blog Post Public Page");
       break;
     case "/manage":
-      console.log("Manage Posts Page");
       managePostsHandler();
       break;
     case "/createpost":
-      console.log("Create Post Page");
       initCreatePost();
       break;
     case "/edit":
-      console.log("Edit Post Page");
       initEditPost();
       break;
     case "/register":
-      console.log("Register Page");
       registerHandler();
       break;
-    default:
-      console.log("Page not found");
   }
 }
 
-// document.addEventListener("DOMContentLoaded", router);
-
 document.addEventListener("DOMContentLoaded", () => {
-  console.info("hey");
   router();
 });
