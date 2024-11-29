f09208429be9c315f75d9b898e3fa43430310d30
anneloviseramstad <160476303+anneloviseramstad@users.noreply.github.com>
2024-11-29 16:50:14 +0100

    attempt at pagination

--- js/api/posts/getPosts.js
+++ js/api/posts/getPosts.js
@@ -1,15 +1,23 @@
 import { GET_ALL_POSTS } from "../../constants/api.js";
 import { headers } from "../../constants/headers.js";
 
-export async function getPosts() {
+let currentPage = 1;
+
+export async function getPosts(limit = 12, tag) {
   try {
-    const response = await fetch(GET_ALL_POSTS, {
+    const params = new URLSearchParams({
+      limit: limit.toString(),
+      page: page.toString(),
+      ...(tag && { tag }),
+    });
+
+    const response = await fetch(`${GET_ALL_POSTS}?${params}`, {
       method: "GET",
       headers: headers(),
     });
 
     if (response.ok) {
-      const { data } = await response.json();
+      const { data, totalPages } = await response.json();
 
       localStorage.setItem("posts", JSON.stringify(data));
 
@@ -24,3 +32,40 @@ export async function getPosts() {
     throw error;
   }
 }
+
+function updatePagination(totalPages) {
+  const paginationContainer = document.querySelector("#paginationContainer");
+
+  paginationContainer.innerHTML = "";
+
+  for (let i = 1; i <= totalPages; i++) {
+    const pageButton = document.createElement("button");
+    pageButton.innerText = i;
+    pageButton.classList.add("page-button");
+    if (i === currentPage) {
+      pageButton.disabled = true;
+    }
+    pageButton.addEventListener("click", () => changePage(i));
+    paginationContainer.appendChild(pageButton);
+  }
+}
+
+function changePage(pageNumber) {
+  currentPage = pageNumber;
+  loadPosts();
+}
+
+async function loadPosts() {
+  const posts = await getPosts();
+  const postsContainer = document.getElementById("postsContainer");
+
+  postsContainer.innerHTML = "";
+
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.className = "post";
+    postElement.innerText = post.title;
+    postsContainer.appendChild(postElement);
+  });
+}
+loadPosts();
